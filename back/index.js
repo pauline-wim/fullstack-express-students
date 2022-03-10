982e6ff8fedc2a77a9e53b8b2d421ac1ca5ae0bf
pauline-wim <linette.wim@hotmail.com>
2022-03-10 12:15:36 +0100

    Add const PORT

---
+++ back/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+
+const students = [
+  {
+    id: 1,
+    name: "Paul",
+  },
+  {
+    id: 2,
+    name: "Bob",
+  },
+  {
+    id: 3,
+    name: "Michael",
+  },
+];
+
+// ROUTES
+
+app.get("/", (_req, res) => {
+  res.send("Students");
+});
+
+app.get("/students", (_req, res) => {
+  res.json(students);
+});
+
+app.post("/students", (req, res) => {
+  //   console.log(req.body);
+  students.push({
+    id: students.length + 1,
+    name: req.body.name,
+  });
+  res.send(students);
+});
+
+// ERROR
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found - 404");
+});
+
+app.listen(PORT, () => console.log("Listening on PORT", PORT));
